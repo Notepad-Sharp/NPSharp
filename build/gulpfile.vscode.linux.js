0bdad249a27ed754a5b84b62d080f418a44fb424
Girelli <sf2943642@gmail.com>
2025-11-29 13:06:09 -0800

    fix do fix

--- build/gulpfile.vscode.linux.js
+++ build/gulpfile.vscode.linux.js
@@ -35,7 +35,8 @@ function getDebPackageArch(arch) {
 }
 
 function prepareDebPackage(arch) {
-	const binaryDir = '../VSCode-linux-' + arch;
+	const binaryDir = path.join(root, 'out', `NPSharp-linux-${arch}`);
+
 	const debArch = getDebPackageArch(arch);
 	const destination = '.build/linux/deb/' + debArch + '/' + product.applicationName + '-' + debArch;
 
@@ -152,7 +153,7 @@ function getRpmPackageArch(arch) {
  * @param {string} arch
  */
 function prepareRpmPackage(arch) {
-	const binaryDir = '../VSCode-linux-' + arch;
+	const binaryDir = path.join(root, 'out', `NPSharp-linux-${arch}`);
 	const rpmArch = getRpmPackageArch(arch);
 	const stripBinary = process.env['STRIP'] ?? '/usr/bin/strip';
 
@@ -248,7 +249,8 @@ function getSnapBuildPath(arch) {
  * @param {string} arch
  */
 function prepareSnapPackage(arch) {
-	const binaryDir = '../VSCode-linux-' + arch;
+	const binaryDir = path.join(root, 'out', `NPSharp-linux-${arch}`);
+
 	const destination = getSnapBuildPath(arch);
 
 	return function () {
